5e441d4fb1349f99a28bd387da8297572511442a
Ezequiel Aceto <ezequiel.aceto@gmail.com>
2018-10-18 16:58:59 -0300

    Add base project

---
+++ sharelock/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  sharelock
+//
+//  Created by Kimi on 18/10/2018.
+//  Copyright © 2018 Auth0. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
